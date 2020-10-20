f6abeecc932ce9f2aef8556a04630ee89d063e84
Saynka <sankakale@gmail.com>
2020-10-19 18:09:41 -0700

    workinglab7

--- server.js
+++ server.js
@@ -14,58 +14,71 @@ const app = express();
 app.use(cors());
 
 //  API Route Definitions
-app.get('/location', handleLocation);
+app.get('/location', locationHandler);
 app.get('/restaurants', restaurantHandler);
 app.get('/places', placesHandler);
 app.get('/weather', handleWeather);
+app.get('/trails', trailHandler);
 app.use('*', notFoundHandler);
 
 
-app.get('/', (request, response) => {
+app.get('/', (req, res) => {
 
-  response.send('Home Page!');
+  res.send('Home Page!');
 });
 
-app.get('/bad', (request, response) => {
+app.get('/bad', (req, res) => {
   throw new Error('poo');
 });
 
 // The callback can be a separate function. Really makes things readable
 app.get('/about', aboutUsHandler);
 
-function aboutUsHandler(request, response) {
-  response.status(200).send('About Us Page');
+function aboutUsHandler(req, res) {
+  res.status(200).send('About Us Page');
 }
 
 // HELPER FUNCTIONS
 
 //
-function handleLocation(request, response) {
-  try {
-    const geoData = require('./data/location.json');
-    const city = request.query.city;
-    const locationData = new Location(city, geoData);
-    response.send(locationData);
-  }
-  catch (error) {
-    console.log('ERROR', error);
-    response.status(500).send('So sorry, something went wrong.');
-  }
+// Route handlers
+//location function=working
+function locationHandler(req, res) {
+  let city = req.query.city;
+  let key = process.env.LOCATIONIQ_API_KEY;
+
+  const URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+
+  superagent.get(URL)
+    .then(data => {
+      let location = new Location(city, data.body[0]);
+      res.status(200).json(location);
+    })
+    .catch((error) => {
+      console.log('error', error);
+      res.status(500).send('Your API call did not work?');
+    });
 }
 
-function Location(city, geoData) {
+function notFoundHandler(req, res) {
+  res.status(404).send('Try again.');
+}
+
+// Constructors
+function Location(city, locationData) {
   this.search_query = city;
-  this.formatted_query = geoData[0].display_name;
-  this.latitude = geoData[0].lat;
-  this.longitude = geoData[0].lon;
+  this.latitude = locationData.lat;
+  this.longitude = locationData.lon;
+  this.formatted_query = locationData.display_name;
 }
 
-function restaurantHandler(request, response) {
+// resturant function
+function restaurantHandler(req, res) {
 
   const url = 'https://developers.zomato.com/api/v2.1/geocode';
   const queryParams = {
-    lat: request.query.latitude,
-    lng: request.query.longitude,
+    lat: req.query.latitude,
+    lng: req.query.longitude,
   };
 
   superagent.get(url)
@@ -77,11 +90,11 @@ function restaurantHandler(request, response) {
       results.nearby_restaurants.forEach(entry => {
         restaurantData.push(new Restaurant(entry));
       });
-      response.send(restaurantData);
+      res.send(restaurantData);
     })
     .catch(() => {
       console.log('ERROR', error);
-      response.status(500).send('So sorry, something went wrong.');
+      res.status(500).send('So sorry, something went wrong.');
     });
 
 }
@@ -92,10 +105,11 @@ function Restaurant(entry) {
   this.locality = entry.restaurant.location.locality;
 }
 
-function placesHandler(request, response) {
+// places function
+function placesHandler(req, res) {
 
-  const lat = request.query.latitude;
-  const lng = request.query.longitude;
+  const lat = req.query.latitude;
+  const lng = req.query.longitude;
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json`;
 
   const queryParams = {
@@ -112,11 +126,11 @@ function placesHandler(request, response) {
       results.features.forEach(entry => {
         places.push(new Place(entry));
       });
-      response.send(places);
+      res.send(places);
     })
     .catch((error) => {
       console.log('ERROR', error);
-      response.status(500).send('So sorry, something went wrong.');
+      res.status(500).send('So sorry, something went wrong.');
     });
 }
 
@@ -126,20 +140,35 @@ function Place(data) {
   this.address = data.place_name;
 }
 
-
-function handleWeather(request, response) {
+// weather function=working
+function handleWeather(req, res) {
   try {
     const data = require('./data/weather.json');
     const weatherData = [];
     data.data.forEach(entry => {
       weatherData.push(new Weather(entry));
     });
-    response.send(weatherData);
+    res.send(weatherData);
   }
   catch (error) {
     console.log('ERROR', error);
-    response.status(500).send('So sorry, something went wrong.');
+    res.status(500).send('So sorry, something went wrong.');
   }
+
+  let forcast = req.query.forcast;
+  let key = process.env.WEATHER;
+
+  const URL = `http://api.weatherbit.io/v2.0/current?key=${key}&q=${forcast}&format=json`;
+
+  superagent.get(URL)
+    .then(data => {
+      let location = new Location(forcast, data.body[0]);
+      res.status(200).json(location);
+    })
+    .catch((error) => {
+      console.log('error', error);
+      res.status(500).send('Your API call did not work?');
+    });
 }
 
 
@@ -148,11 +177,41 @@ function Weather(obj) {
   this.time = new Date(obj.valid_date).toDateString();
 }
 
-function notFoundHandler(request, response) {
-  response.status(404).send('huh?');
+// trail function=working
+function trailHandler(req, res) {
+  let lat = req.query.latitude;
+  let lon = req.query.longitude;
+  let key = process.env.HIKING;
+  let url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${key}`;
+
+  superagent.get(url)
+    .then(hike => {
+      const hikingData = hike.body.trails;
+      const trailData = hikingData.map(active => new Hiking(active));
+      res.send(trailData);
+    });
+    .catch(() => response.status(500).send('So sorry, something went wrong.'));
+}
+
+function notFoundHandler(req, res) {
+  res.status(404).send('Try again.');
+}
+function Hiking(active) {
+  this.name = active.name;
+  this.location = active.location;
+  this.length = active.length;
+  this.stars = active.stars;
+  this.star_votes = active.starVotes;
+  this.summary = active.summary;
+  this.trail_url = active.url;
+  this.conditions = active.conditionDetails;
+  this.condition_date = active.conditionDate.slice(0, 9);
+  this.condition_time = active.conditionDate.slice(11, 19);
 }
 
 
 
-// Make sure the server is listening for requests
+
+
+// Make sure the server is listening for reqs
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
